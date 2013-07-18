e5c8809c4682e541f911c43e1172548a3f94ab03
andreczip <andreczip@gmail.com>
2013-07-18 18:00:16 -0300

    first commit

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+clear
+echo "Hello $USER to press any key to start the installation"
+
+read $tecla
+		clear
+		echo "Enter your password for the root user"
+        
+        echo "Installing GIT"
+        sudo apt-get install git
+        clear
+
+        echo "Installing Apache and PHP5"
+		sudo apt-get install apache2 &&
+		sudo apt-get install php5 libapache2-mod-php5 &&
+		clear
+
+		echo "Installing MySQL"
+		sleep 2	
+        sudo apt-get install mysql-server &&
+		sudo apt-get install libapache2-mod-auth-mysql php5-mysql &&
+		sudo /etc/init.d/apache2 restart
+		clear
+
+        echo "Installing Sublime Text"
+        sudo add-apt-repository ppa:webupd8team/sublime-text-2
+        sudo apt-get update
+        sudo apt-get install sublime-text
+        clear
+	
+echo "The End!"
+		
+
